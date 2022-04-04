24a828551f16c1334d50f28b1ee6a5a66e775561
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-04-04 10:55:31 -0700

    did challenge oddishOrEvenish

--- challenges/challenge1.js
+++ challenges/challenge1.js
@@ -6,3 +6,13 @@ function reverseWords(sentence) {
     })
     .join(" ");
 }
+
+function oddishOrEvenish(num) {
+  const sumDigits = num.toString().split("");
+  const add = sumDigits.map((n) => +n).reduce((a, b) => a + b);
+  if (add % 2 === 0) {
+    return "evenish";
+  } else {
+    return "oddish";
+  }
+}
